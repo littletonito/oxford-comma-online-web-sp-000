c06a85c6d96e0ccb75411e7596292da21f1d5270
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-09-25 00:29:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.size == 2
     array.join(" and ")
   elsif array.size == 3
-    array[-1].insert(-2, " and ")
+    array[-1].insert(0, " and ")
   else
     array.join(", ")
   end
